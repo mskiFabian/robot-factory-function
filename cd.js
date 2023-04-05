6df04a44c52f62d9450d2592eb75ac6e9e8dc35b
Fabian <125907927+mskiFabian@users.noreply.github.com>
2023-04-05 17:55:22 +0200

    Add files via upload

---
+++ cd.js
@@ -0,0 +1,16 @@
+// create a robot factory function 
+
+const robotFactory = (model, mobile) => {
+    return {
+      model: model,
+      mobile: mobile,
+      beep() {
+      console.log('Beep Boop')
+    }
+    }  
+  }
+  const tinCan = robotFactory('P-500', true)
+  tinCan.beep()
+
+
+
